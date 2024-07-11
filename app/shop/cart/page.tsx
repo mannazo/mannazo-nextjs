6224f3e574eee2c832e414cbe1d76001c721f2a3
hl6vbw <hl6vbw@virginia.edu>
2024-07-11 21:58:41 +0900

    last one for today

--- app/shop/cart/page.tsx
+++ app/shop/cart/page.tsx
@@ -1,3 +1,82 @@
-export default function Page({ params }: { params: { slug: string } }) {
-  return <div>slug: {params.slug}</div>
+'use client'
+
+import { useEffect, useState } from 'react'
+import { ShoppingCart } from '@/_model/shoppingCart'
+import {Product } from '@/_model/product'
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  Button,
+} from '@nextui-org/react'
+
+
+export default function Page() {
+  const [cart,setCart] = useState<ShoppingCart>({ items:[] })
+  useEffect(() => {
+    const storedCart = JSON.parse(localStorage.getItem('cart') || '{"items": []}');
+    setCart(storedCart);
+  }, []);
+
+  const removeFromCart = (productId: number) => {
+    setCart(prevCart => {
+      const updatedItems = prevCart.items.filter(item => item.product.product_id !== productId);
+      const updatedCart = { ...prevCart, items: updatedItems };
+      localStorage.setItem('cart', JSON.stringify(updatedCart));
+      return updatedCart;
+    });
+  };
+
+  const totalPrice = cart.items.reduce((total, item) => total + item.product.price * item.quantity, 0);
+
+  return(
+    <>
+    <h1>Shopping Cart</h1>
+      {cart.items.length > 0 ? (
+        <Table aria-label="Shopping Cart" shadow={false} lined headerLined>
+          <Table.Header>
+            <Table.Column>PRODUCT</Table.Column>
+            <Table.Column>DESCRIPTION</Table.Column>
+            <Table.Column>PRICE</Table.Column>
+            <Table.Column>QUANTITY</Table.Column>
+            <Table.Column>ACTION</Table.Column>
+          </Table.Header>
+          <Table.Body>
+            {cart.items.map(({ product, quantity }) => (
+              <Table.Row key={product.product_id}>
+                <Table.Cell>{product.name}</Table.Cell>
+                <Table.Cell>{product.description}</Table.Cell>
+                <Table.Cell>${product.price}</Table.Cell>
+                <Table.Cell>{quantity}</Table.Cell>
+                <Table.Cell>
+                  <Button flat color="error" onPress={() => removeFromCart(product.product_id)}>
+                    Remove
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+          <Table.Footer>
+            <Table.Row>
+              <Table.Cell colSpan={4} css={{ textAlign: 'right' }}>
+                <h4>Total: ${totalPrice.toFixed(2)}</h4>
+              </Table.Cell>
+              <Table.Cell>
+
+                <Button auto color="primary">
+                  Proceed to Checkout
+                </Button>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Footer>
+        </Table>
+      ) : (
+        <h3>Your cart is empty.</h3>
+      )}
+    </>
+  )
 }
--- app/shop/shop/productCards.tsx
+++ app/shop/shop/productCards.tsx
@@ -5,6 +5,7 @@ import ProductCard from './productCard'
 import {Product} from '@/_model/product'
 import { ShoppingCart } from '@/_model/shoppingCart'
 import ShoppingCartIcon from '../shop/shoppingCartIcon'
+import Link from 'next/link'
 
 const ProductCards : React.FC = () => {
   const [products, setProducts] = useState<Product[]> ([]);
@@ -19,6 +20,7 @@ const ProductCards : React.FC = () => {
   }, []);
 
   const addToCart = (product: Product ) => {
+    // add to cart
     setCart(prevCart => {
       return {
         ...prevCart,
@@ -26,12 +28,19 @@ const ProductCards : React.FC = () => {
       };
     })
     console.log(cart)
+    // update cart to localStorage for access from shooping car
+    localStorage.setItem('cart', JSON.stringify(cart))
+  //   localStorage is always stored as strings (JSON.stringify). so should be retrieved with JSOn.parse() to get the object form
+
   }
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold mb-8">Featured Products</h1>
-      <ShoppingCartIcon cart={cart} />
+      <Link href="/shop/cart">
 
+          <ShoppingCartIcon cart={cart} />
+
+      </Link>
       <div className="flex flex-wrap justify-between">
         {products.map(product => (
           <div key={product.product_id} className="w-full sm:w-1/2 lg:w-1/4 p-2">
